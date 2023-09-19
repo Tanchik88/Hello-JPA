3407e29212191a4a36f33e6fb43a6a2dc5d7c59f
Tetiana Chernobai <yzozulya@my.com>
2023-09-19 14:09:44 +0200

    add entity City

---
+++ src/main/java/spring/domain/City.java
@@ -0,0 +1,26 @@
+package spring.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "cities")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+
+}
